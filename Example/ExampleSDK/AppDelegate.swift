b74b124e1acbe4860bf5a4b9e38505cfe8118df9
Mayank Verma <workatmayank@gmail.com>
2023-05-24 16:19:21 +0530

    Initial Commit

--- Example/ExampleSDK/AppDelegate.swift
+++ Example/ExampleSDK/AppDelegate.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import ExampleSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var sdkExample: SDKSource = SDKSource.init()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        sdkExample.helloworld()
         // Override point for customization after application launch.
         return true
     }
---
+++ SDKSource.swift
@@ -0,0 +1,16 @@
+//
+//  SDKSource.swift
+//  ExampleSDK
+//
+//  Created by Mayank on 24/05/23.
+//
+
+import Foundation
+
+public class SDKSource: NSObject {
+    
+    public func helloworld() {
+        print("Hello World")
+    }
+    
+}
